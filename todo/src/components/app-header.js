3a7a6439b2d333a5e285da59e4ef59258f9f5786
ovdink <fake@mail.com>
2019-11-08 17:14:38 +0300

    structure updated

--- todo/src/components/app-header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AppHeader = () => {
-    return <h1 className='h1 m-3 text-center'>Todo List</h1>;
-};
-
-export default AppHeader;
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header'
-import SearchPanel from './components/search-panel'
-import TodoList from './components/todo-list'
-
-const App = () => {
-
-    const isLoggedIn = true;
-    const loginBox = <span>Log in please</span>;
-    const welcomeBox = <span>Welcome Back</span>;
-
-    return (
-        <div>
-            {isLoggedIn ? welcomeBox : loginBox}
-            <AppHeader />
-            <SearchPanel />
-            <TodoList />
-        </div>
-    );
-};
+import App from './components/app';
 
 ReactDOM.render(<App />,
-    document.getElementById('root'));
+  document.getElementById('root'));
